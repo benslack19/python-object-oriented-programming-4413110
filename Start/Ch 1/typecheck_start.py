4aad3fca4239ccd8cebefe190d96a7409c2ae1aa
Ben Lacar <blacar@seer.bio>
2024-03-03 20:43:14 -0800

    Ch1: checking instance types

--- Start/Ch 1/typecheck_start.py
+++ Start/Ch 1/typecheck_start.py
@@ -19,9 +19,19 @@ n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
 
 # TODO: use type() to inspect the object type
-
+print(type(b1))
+print(type(n1))
 
 # TODO: compare two types together
-
+print(type(b1) == type(b2))
+print(type(b1) == print(type(n2)))
 
 # TODO: use isinstance to compare a specific instance to a known type
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(n2, Book))
+
+
+# you can also do this on a subclass
+# every object is a subclass of a built-in python object class
+print(isinstance(n2, object))
