33ce1529f7aed4da230eaeb599955357dea76dca
Ben Lacar <blacar@seer.bio>
2024-04-01 08:18:55 -0700

    Ch3: attribute access

--- Start/Ch 2/inheritance_start.py
+++ Start/Ch 2/inheritance_start.py
@@ -21,29 +21,39 @@ class Publication:
 
 # create new class Periodical for period, publisher duplication
 class Periodical(Publication):
-    def __init__(
-        self, title, period, publisher, price
-    ):  # what if you change the order? it doesn't matter
+    def __init__(self, title, period, publisher, price):
+
         super().__init__(title, price)
+        # self.title = title + " periodical"
+        title = title + " periodical"
         self.period = period
         self.publisher = publisher
 
-    def print_title(self, publisher):
-        print(f"The title is {self.title}")
+    # def print_title(self, publisher):
+    #     print(f"The title is {self.title}")
+
+
+pub1 = Publication("NYtimes", 1.99)
+per1 = Periodical("SI", "Monthly", "Time", 5.99)
+print(pub1.title, pub1.price)
+print(pub1.print_title())
+print(per1.title, per1.period, per1.publisher, per1.price)
+per1.print_title()
+
+
+# p2 = Periodical_no_price("SI", 3.40, "Monthly", "Time")
 
 
 # what if you don't use all attributes in super().__init__ and add others?
-class Periodical_no_price(Publication):
-    def __init__(
-        self, title, period, publisher
-    ):  # what if you change the order? it doesn't matter
-        super().__init__(title)
-        self.period = period
-        self.publisher = publisher
+# class Periodical_no_price(Publication):
+#     def __init__(self, title, period, publisher):
+#         super().__init__(title)
+#         self.period = period
+#         self.publisher = publisher
 
 
 class Book(Publication):  # have it inherit
-    def __init__(self, title, author, pages, price):
+    def __init__(self, title, price, author, pages):
         super().__init__(title, price)
         # no longer need self.title and self.price
         # self.title = title
@@ -73,14 +83,17 @@ class Newspaper(Periodical):  # like Magazine
         # self.publisher = publisher
 
 
-p1 = Periodical("SI", "Monthly", "Time", 5.99)
-p2 = Periodical_no_price("SI", "Monthly", "Time")
-b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
-n1 = Newspaper("NY Times", "New York Times Company", 6.0, "Daily")
-m1 = Magazine("Scientific American", "Springer Nature", 5.99, "Monthly")
+# p1 = Periodical("SI", "Monthly", "Time", 5.99)
 
-print(p1.title, p1.period, p1.publisher, p1.price)
-print("Periodical no price: ", p2.title, p2.period, p2.publisher)
-print(b1.author)
-print(n1.publisher)
-print(b1.price, m1.price, n1.price)
+# # p2 = Periodical_no_price("SI", 3.40, "Monthly", "Time")
+# b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
+# n1 = Newspaper("NY Times", "New York Times Company", 6.0, "Daily")
+# m1 = Magazine("Scientific American", "Springer Nature", 5.99, "Monthly")
+
+# print(p1.title, p1.period, p1.publisher, p1.price)
+# # print("printing title: ", p1.print_title("Time"))
+
+# # print("Periodical no price: ", p2.title, p2.period, p2.publisher)
+# print(b1.author)
+# print(n1.publisher)
+# print(b1.price, m1.price, n1.price)
--- Start/Ch 3/magicattr_start.py
+++ Start/Ch 3/magicattr_start.py
@@ -17,13 +17,38 @@ class Book:
 
     # TODO: __getattribute__ called when an attr is retrieved. Don't
     # directly access the attr name otherwise a recursive loop is created
+    def __getattribute__(self, name):
+        """Alter the price attribute to include a discount"""
+        if name == "price":
+            p = super().__getattribute__("price")
+            d = super().__getattribute__("_discount")
+            p = p - p * d
+            return p
+        # if it's price, then return the normal attribute
+        return super().__getattribute__(name)
 
     # TODO: __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
+    def __setattr__(self, name, value):
+        """Enforce that the price is a float"""
+        if name == "price":
+            if type(value) is not float:
+                raise ValueError("The 'price' attr must be a float")
+        # still need the other branch to let other attributes be set
+        return super().__setattr__(name, value)
 
     # TODO: __getattr__ called when __getattribute__ lookup fails - you can
     # pretty much generate attributes on the fly with this method
+    # only gets called if __getattribute__isn't defined, if attribute doesn't exist, or throws exception
+    # like others, be careful about not entering a recursive loop
+    def __getattr__(self, name):
+        return name + " does not exist"
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+b1.price = float(40)
+print(b1)
+# note how the printed price is lower than the set price and other attributes aren't affected
+print(b1.randomprp)
