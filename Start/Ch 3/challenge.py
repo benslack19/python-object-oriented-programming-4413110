6704923b8b47b0aefd10c6ff05affc900d5bf7fe
Ben Lacar <blacar@seer.bio>
2024-04-05 21:27:04 -0700

    Ch3: magiccall and challenge

--- Start/Ch 3/challenge.py
+++ Start/Ch 3/challenge.py
@@ -16,12 +16,19 @@ class Asset(ABC):
     def __str__(self):
         pass
 
-
 class Stock(Asset):
     def __init__(self, ticker, price, company):
         super().__init__(price)
         self.company = company
         self.ticker = ticker
+
+    def __str__(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+
+    def __lt__(self, value):
+        if not isinstance(value, Stock):
+            raise ValueError("Can't compare to a non-Stock object")
+        return self.price < value.price
 
 
 class Bond(Asset):
@@ -30,6 +37,14 @@ class Bond(Asset):
         self.description = description
         self.duration = duration
         self.yieldamt = yieldamt
+
+    def __str__(self):
+        return f"{self.description}: {self.duration}'yr' : ${self.price} : {self.yieldamt}%"
+
+    def __lt__(self, value):
+        if not isinstance(value, Bond):
+            raise ValueError("Can't compare to a non-Bond object")
+        return self.yieldamt < value.yieldamt
 
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
